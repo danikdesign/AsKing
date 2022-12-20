813899badffa251852fab87439cbe44ca377d050
danikdesign <danik.newmail@gmail.com>
2022-12-20 18:55:01 +0200

    Added paginator for questions and answers

--- app/controllers/answers_controller.rb
+++ app/controllers/answers_controller.rb
@@ -1,4 +1,5 @@
 class AnswersController < ApplicationController
+  include ActionView::RecordIdentifier
   before_action :set_question!
   before_action :set_answer!, only: %i[edit update destroy]
 
@@ -22,7 +23,7 @@ class AnswersController < ApplicationController
 
   def update
     if @answer.update answer_params
-      redirect_to question_path(@question, anchor: "answer-#{@answer.id}")
+      redirect_to question_path(@question, anchor: dom_id(@answer))
     else
       render :edit, status: :unprocessable_entity
     end
