84383529f66ddd02a0674412ff8286ebdafbaf05
danikdesign <danik.newmail@gmail.com>
2022-12-21 18:15:06 +0200

    Added logout for users

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,7 @@
 class UsersController < ApplicationController
+  before_action :require_no_authentication, only: %i[new create]
+  before_action :require_authentication, only: %i[edit update]
+  before_action :set_user!, only: %i[edit update]
   def new
     @user = User.new
   end
@@ -6,7 +9,7 @@ class UsersController < ApplicationController
   def create
     @user = User.new user_params
     if @user.save
-      session[:user_id] = @user.id
+      sign_in(@user)
       flash[:success] = "Welcome to the app, #{current_user.name_or_email}"
       redirect_to root_path
     else
@@ -14,8 +17,23 @@ class UsersController < ApplicationController
     end
   end
 
+  def edit
+  end
+
+  def update
+    if @user.update user_params
+      flash[:success] = "Your profile was successfully updated!"
+      redirect_to edit_user_path(@user)
+    else
+      render :edit
+    end
+  end
+
   private
 
+  def set_user!
+    @user = User.find params[:id]
+  end
   def user_params
     params.require(:user).permit(:email, :name, :password, :password_confirmation)
   end
