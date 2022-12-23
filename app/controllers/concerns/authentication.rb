0da7f787b05cdd452f7b4b5b4d073881de7b34d7
danikdesign <danik.newmail@gmail.com>
2022-12-23 23:33:13 +0200

    Added remember_me functional

--- app/controllers/concerns/authentication.rb
+++ app/controllers/concerns/authentication.rb
@@ -6,8 +6,17 @@ module Authentication
 
     private
     def current_user
-      @current_user ||= User.find_by(id: session[:user_id]).decorate if session[:user_id].present?
+      if session[:user_id].present?
+        @current_user ||= User.find_by(id: session[:user_id]).decorate
+      elsif cookies[:user_id].present?
+        user = User.find_by(id: session[:user_id]).decorate
+        if user&.remember_token_autheticated?(cookies.encrypted[:remember_token])
+          sign_in(user)
+          @current_user ||= user.decorate
+        end
+      end
     end
+
 
     def user_signed_in?
       current_user.present?
@@ -18,6 +27,7 @@ module Authentication
     end
 
     def sign_out
+      forget current_user
       session.delete :user_id
     end
 
@@ -33,6 +43,18 @@ module Authentication
 
       flash[:warning] = "You are not signed in!"
       redirect_to root_path
+    end
+
+    def remember(user)
+      user.remember_me
+      cookies.encrypted.permanent[:remember_token] = user.remember_token
+      cookies.encrypted.permanent[:user_id] = user.id
+    end
+
+    def forget(user)
+      user.forget_me
+      cookies.delete[:user_id]
+      cookies.delete[:remember_token]
     end
 
     helper_method :current_user
