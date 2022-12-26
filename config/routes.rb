a672cb9757e06c2213071910781b7720774195fa
danikdesign <danik.newmail@gmail.com>
2022-12-26 18:53:59 +0200

    Created routes for admin

--- config/routes.rb
+++ config/routes.rb
@@ -6,6 +6,10 @@ Rails.application.routes.draw do
     resources :answers, only: %i[create destroy edit update]
   end
 
+  namespace :admin do
+    resources :users, only: %i[index]
+  end
+
   root 'pages#index'
 
 end
