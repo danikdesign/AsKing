b7985c02f9faa251a39287630a86a4316e1a2bd6
danikdesign <danik.newmail@gmail.com>
2022-12-26 20:59:59 +0200

    Created users_from method

--- app/services/user_bulk_service.rb
+++ app/services/user_bulk_service.rb
@@ -8,8 +8,23 @@ class UserBulkService < ApplicationService
   def call
     Zip::File.open(@archive) do |zip_file|
       zip_file.each do |entry|
-
+        User.import users_from(entry), ignore: true
       end
     end
   end
+
+  private
+
+  def users_from(entry)
+    sheet = RubyXL::Parser.parse_buffer(entry.get_input_stream.read)[0]
+    sheet.map do |row|
+      cells = row.cells
+      User.new name: cells[0]&.value.to_s,
+               email: cells[1]&.value.to_s,
+               password: cells[2]&.value.to_s,
+               password_confirmation: cells[2]&.value.to_s
+    end
+  end
+
+
 end
