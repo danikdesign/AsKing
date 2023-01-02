cea588ebd618d1ebcd5cde7a24d256dec2a959f0
danikdesign <danik.newmail@gmail.com>
2023-01-02 12:54:51 +0200

    Update style for gravatar view

--- app/decorators/user_decorator.rb
+++ app/decorators/user_decorator.rb
@@ -8,9 +8,7 @@ class UserDecorator < ApplicationDecorator
   end
 
   def gravatar(size: 30, css_class: '')
-    email_hash = Digest::MD5.hexdigest email.strip.downcase
-
-    h.image_tag "https://gravatar.com/avatar/#{email_hash}.jpg?s=#{size}", class: "rounded #{css_class}, alt: name_or_email"
+    h.image_tag "https://gravatar.com/avatar/#{gravatar_hash}.jpg?s=#{size}", class: "rounded-circle #{css_class}, alt: name_or_email"
   end
 
 end
