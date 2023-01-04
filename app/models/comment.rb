9ae8c1402a71b156e134775d514e8334961e3a1b
danikdesign <danik.newmail@gmail.com>
2023-01-05 00:12:09 +0200

    Added for? method to comment model

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -3,4 +3,10 @@ class Comment < ApplicationRecord
   belongs_to :user
 
   validates :body, presence: true, length: {minimum: 2}
+
+  def for?(commentable)
+    commentable = commentable.object if commentable.decorated?
+
+    commentable == self.commentable
+  end
 end
