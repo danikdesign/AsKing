da64f320a414525fc8b205108133dbdb0a18ac53
danikdesign <danik.newmail@gmail.com>
2023-01-04 23:34:08 +0200

    Created set_commentable method for determine the class to which the comment belongs

---
+++ app/controllers/comments_controller.rb
@@ -0,0 +1,32 @@
+class CommentsController < ApplicationController
+
+  include QuestionsAnswers
+  before_action :set_commentable!
+  before_action :set_question
+  def create
+    @comment = @commentable.comments.build comment_params
+
+    if @comment.save
+      flash[:success] = 'Comment created'
+      redirect_to question_path(@question)
+    end
+  end
+
+  private
+
+  def comment_params
+    params.require(:comment).permit(:body).merge(user: current_user)
+  end
+
+  def set_commentable!
+    klass = [Question, Answer].detect {|c| params["#{c.name.underscore}_id"]}
+
+    raise ActiveRecord::RecordNotFound if klass.blank?
+
+    @commentable = klass.find(params["#{klass.name.underscore}_id"])
+  end
+
+  def set_question
+    @question = @commentable.is_a?(Question) ? @commentable : @commentable.question
+  end
+end
