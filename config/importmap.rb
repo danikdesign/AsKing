c073717048d44f656d4628c76232fa6f85698e9f
danikdesign <danik.newmail@gmail.com>
2023-01-06 18:45:28 +0200

    added 'slim-select'

--- config/importmap.rb
+++ config/importmap.rb
@@ -2,3 +2,4 @@
 
 pin "application", preload: true
 pin "@hotwired/turbo-rails", to: "turbo.min.js", preload: true
+pin "slim-select", to: "https://ga.jspm.io/npm:slim-select@2.3.3/dist/slimselect.umd.js"
