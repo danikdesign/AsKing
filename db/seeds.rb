d34d15b2928b26c0196e599d744359b613dc5bf3
danikdesign <danik.newmail@gmail.com>
2023-01-06 14:54:51 +0200

    Deleting questions generator by FFaker

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,9 +1,3 @@
-#30.times do
-#  title = FFaker::HipsterIpsum.words
-#  body = FFaker::Lorem.paragraphs
-#  Question.create title: title.to_s, body: body.to_s
-#end
-
 User.each { |u| u.send(:set_gravatar_hash) ; u.save }
 
 
