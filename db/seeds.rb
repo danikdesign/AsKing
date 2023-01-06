0bcb83cc6dfa2401c7ae6ea7d5d3aecf91ff9e03
danikdesign <danik.newmail@gmail.com>
2023-01-06 18:28:18 +0200

    Added random tags

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,3 +1,14 @@
-User.each { |u| u.send(:set_gravatar_hash) ; u.save }
+#30.times do
+#  title = FFaker::HipsterIpsum.words
+#  body = FFaker::Lorem.paragraphs
+#  Question.create title: title.to_s, body: body.to_s
+#end
+
+#User.each { |u| u.send(:set_gravatar_hash) ; u.save }
+
+30.times do
+  title = FFaker::HipsterIpsum.word
+  Tag.create title: title
+end
 
 
