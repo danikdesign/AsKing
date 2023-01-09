fe2fce0c05d973c4c9680dd24387c2787bc1d794
danikdesign <danik.newmail@gmail.com>
2023-01-10 00:22:13 +0200

    Implementation of authorization methods for interactions with questions

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -2,6 +2,9 @@ class QuestionsController < ApplicationController
   include QuestionsAnswers
   before_action :set_question!, only: %i[show edit update destroy]
   before_action :fetch_tags, only: %i[new edit]
+  before_action :require_authentication, except: %i[index show]
+  before_action :authorize_question!
+  after_action :verify_authorized
   def index
     @pagy, @questions = pagy Question.all_by_tags(params[:tag_ids])
     @questions = @questions.decorate
@@ -54,5 +57,9 @@ class QuestionsController < ApplicationController
 
   def fetch_tags
     @tags = Tag.all
+  end
+
+  def authorize_question!
+    authorize(@question || Question)
   end
 end
