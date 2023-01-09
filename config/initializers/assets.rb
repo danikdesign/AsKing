5bf76f316fd9baebd32b6fc49f2fa735b6d5d2db
danikdesign <danik.newmail@gmail.com>
2023-01-09 22:27:30 +0200

    Fixed bug with js

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -4,8 +4,6 @@
 Rails.application.config.assets.version = "1.0"
 
 Rails.application.config.assets.precompile += %w( bootstrap.min.js popper.js )
-
-Rails.application.config.assets.paths << "app/javascript/controllers"
 # Add additional assets to the asset load path.
 # Rails.application.config.assets.paths << Emoji.images_path
 
