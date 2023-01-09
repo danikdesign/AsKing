d58d08a813bc718953babc404692a30d587ec5b4
danikdesign <danik.newmail@gmail.com>
2023-01-09 22:29:48 +0200

    Created users_helper with user_roles method for viewing user role on user edit page

--- app/helpers/admin/users_helper.rb
+++ app/helpers/admin/users_helper.rb
@@ -1,2 +1,9 @@
-# frozen_string_literal: true
-
+module Admin
+  module UsersHelper
+    def user_roles
+      User.roles.keys.map do |role|
+        [ role.titleize, role ]
+      end
+    end
+  end
+end
