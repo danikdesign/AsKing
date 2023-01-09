563a35c38a55f0155fec62a06d790c85a0d3147e
danikdesign <danik.newmail@gmail.com>
2023-01-10 00:23:51 +0200

    The authorship concern was created. Connected to question, answer, and comment models

--- app/models/answer.rb
+++ app/models/answer.rb
@@ -1,5 +1,6 @@
 class Answer < ApplicationRecord
   include Commentable
+  include Authorship
 
   belongs_to :question
   belongs_to :user
--- app/models/concerns/authorship.rb
+++ app/models/concerns/authorship.rb
@@ -1,2 +1,9 @@
-# frozen_string_literal: true
+module Authorship
+  extend ActiveSupport::Concern
 
+  included do
+    def authored_by?(user)
+      self.user == user
+    end
+  end
+end
