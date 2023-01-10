d34d385a162db25be02a96e9c8be8a660b1b733c
danikdesign <danik.newmail@gmail.com>
2023-01-10 23:12:12 +0200

    Added user mailer for send email about users import results

--- app/mailers/admin/user_mailer.rb
+++ app/mailers/admin/user_mailer.rb
@@ -1,2 +1,16 @@
-# frozen_string_literal: true
+module Admin
+  class UserMailer < ApplicationMailer
+    def bulk_import_done
+      @user = params[:user]
 
+      mail to: @user.email, subject: 'SUCCESS! Bulk import of users'
+    end
+
+    def bulk_import_fail
+      @error = params[:error]
+      @user = params[:user]
+
+      mail to: @user.email, subject: 'ERROR! Bulk import of users'
+    end
+  end
+end
