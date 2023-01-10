e3eb5492871938896fa5f79ee14f638c0449d1ee
danikdesign <danik.newmail@gmail.com>
2023-01-10 13:44:58 +0200

    Created concern for creating password reset tokens

--- app/models/concerns/recoverable.rb
+++ app/models/concerns/recoverable.rb
@@ -1,2 +1,10 @@
-# frozen_string_literal: true
+module Recoverable
+  extend ActiveSupport::Concern
 
+  included do
+    def set_password_reset_token
+      update password_reset_token: digest(SecureRandom.urlsafe_base64),
+             password_reset_token_sent_at: Time.current
+    end
+  end
+end
