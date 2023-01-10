55cc0ed4352e49c5a00ec3287d427bd94a660f72
danikdesign <danik.newmail@gmail.com>
2023-01-10 13:44:25 +0200

    Added password reset token to create password reset method

--- app/controllers/password_resets_controller.rb
+++ app/controllers/password_resets_controller.rb
@@ -2,7 +2,15 @@ class PasswordResetsController < ApplicationController
   before_action :require_no_authentication
 
   def create
-    PasswordResetMailer.with(user: @user).reset_email.deliver_later
+    @user = User.find_by email: params[:email]
+    if @user.present?
+      @user.set_password_reset_token
+      PasswordResetMailer.with(user: @user).reset_email.deliver_later
+    end
+
+    flash[:success] = 'Check your email for instructions'
+    redirect_to new_session_path
+
   end
 end
 
