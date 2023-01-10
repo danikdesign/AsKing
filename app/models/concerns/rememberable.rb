6f051378a5e924fa607ae98428292a50e9e2756d
danikdesign <danik.newmail@gmail.com>
2023-01-10 14:47:12 +0200

    Created Rememberable concern for remember_me methods

--- app/models/concerns/rememberable.rb
+++ app/models/concerns/rememberable.rb
@@ -1,2 +1,23 @@
-# frozen_string_literal: true
+module Rememberable
+  extend ActiveSupport::Concern
 
+  included do
+    attr_accessor :remember_token
+
+    def remember_me
+      self.remember_token = SecureRandom.urlsafe_base64
+      update_column :remember_token_digest, digest(remember_token)
+    end
+
+    def forget_me
+      update_column :remember_token_digest, nil
+      self.remember_token = nil
+    end
+
+    def remember_token_authenticated?(remember_token)
+      return false unless remember_token_digest.present?
+      BCrypt::Password.new(remember_token_digest).is_password?(remember_token)
+    end
+
+  end
+end
