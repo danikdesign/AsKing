82ed4cba680566ff5001ad070802ebd071cfedef
danikdesign <danik.newmail@gmail.com>
2023-01-10 14:45:05 +0200

    Created methods for password update in password resets controller

--- app/controllers/password_resets_controller.rb
+++ app/controllers/password_resets_controller.rb
@@ -1,6 +1,6 @@
 class PasswordResetsController < ApplicationController
   before_action :require_no_authentication
-
+  before_action :set_user, only: %i[edit update]
   def create
     @user = User.find_by email: params[:email]
     if @user.present?
@@ -11,6 +11,33 @@ class PasswordResetsController < ApplicationController
     flash[:success] = 'Check your email for instructions'
     redirect_to new_session_path
 
+  end
+
+  def edit
+  end
+
+  def update
+    if @user.update user_params
+      flash[:success] = 'Your password has been reset'
+      redirect_to new_session_path
+    else
+      render :edit, status: :unprocessable_entity
+    end
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit(:password, :password_confirmation).merge(skip_old_password: true)
+  end
+
+  def set_user
+    redirect_to(new_session_path, flash: { warning: 'Password resets fail' }) and return unless params[:user].present?
+
+    @user = User.find_by email: params[:user][:email],
+                         password_reset_token: params[:user][:password_reset_token]
+
+    redirect_to(new_session_path, flash: { warning: 'Password resets fail' }) unless @user&.password_reset_period_valid?
   end
 end
 
