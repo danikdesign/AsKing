1d366002c156426fd0d5f276c574b40c080851ba
danikdesign <danik.newmail@gmail.com>
2023-01-10 13:26:13 +0200

    Created password reset mailer

--- app/mailers/password_reset_mailer.rb
+++ app/mailers/password_reset_mailer.rb
@@ -1,2 +1,7 @@
-# frozen_string_literal: true
+class PasswordResetMailer < ApplicationMailer
+  def reset_email
+    @user = params[:user]
 
+    mail to: @user.email, subject: 'Password reset | AsKing'
+  end
+end
