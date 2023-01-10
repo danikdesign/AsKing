caa19c08df4da77b91f666d17a53ad9c76e50c09
danikdesign <danik.newmail@gmail.com>
2023-01-10 13:25:57 +0200

    Added create method to password resets controller

--- app/controllers/password_resets_controller.rb
+++ app/controllers/password_resets_controller.rb
@@ -1,2 +1,8 @@
-# frozen_string_literal: true
+class PasswordResetsController < ApplicationController
+  before_action :require_no_authentication
+
+  def create
+    PasswordResetMailer.with(user: @user).reset_email.deliver_later
+  end
+end
 
